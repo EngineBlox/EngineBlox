59a2f2956dad53dbd086eb465bf4e3b8d77de387
Arran Ewing <arran.ewing@iceland.co.uk>
2021-06-11 12:16:19 +0100

    Add JsonApiMock

---
+++ EngineBlox.Test.Utility/Api/JsonApiMock.cs
@@ -0,0 +1,24 @@
+﻿using EngineBlox.Api;
+using Moq;
+using Newtonsoft.Json;
+using System;
+using System.Linq.Expressions;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace EngineBlox.Test.Utility.Api
+{
+    public static class JsonApiMock
+    {
+        public static Mock<IJsonApi<TApi>> ReturnDefault<TApi, TResult>(Expression<Func<IJsonApi<TApi>, Task<HttpResponseMessage>>> methodSetup) where TResult : new()
+        {
+            var api = new Mock<IJsonApi<TApi>>();
+            var message = new HttpResponseMessage
+            {
+                Content = new StringContent(JsonConvert.SerializeObject(new TResult()))
+            };
+            api.Setup(methodSetup).ReturnsAsync(message);
+            return api;
+        }
+    }
+}
