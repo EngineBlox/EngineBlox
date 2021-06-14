93b107a108e6d6b4d3510893818d5e535897c4d3
Arran Ewing <arran.ewing@iceland.co.uk>
2021-06-14 11:34:21 +0100

    Add action result extensions test utility

---
+++ EngineBlox.Test.Utility/Api/ActionResultExtensions.cs
@@ -0,0 +1,19 @@
+﻿using EngineBlox.Azure.Functions;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+
+namespace EngineBlox.Test.Utility.Api
+{
+    public static class ActionResultExtensions
+    {
+        public static TResult GetSerialisedJsonReponse<TResult>(this IActionResult response)
+        {
+            var jsonResult = response.Should().BeOfType<SerialisedJsonResult>().Subject;
+
+            return JsonConvert.DeserializeObject<TResult>(jsonResult.Content)
+                ?? throw new Exception($"Expected to deserialise {typeof(TResult).Name}");
+        }
+    }
+}
